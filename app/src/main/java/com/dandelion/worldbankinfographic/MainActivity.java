9ba33925eff9ea6f894157194ea17cc689e2e785
Mert Ruso <mert.ruso@kcl.ac.uk>
2015-11-23 15:17:59 +0000

    commit test

--- app/src/main/java/com/dandelion/worldbankinfographic/MainActivity.java
+++ app/src/main/java/com/dandelion/worldbankinfographic/MainActivity.java
@@ -14,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
 
         System.out.println("committ test numbe 2");
         System.out.println("commit test number 3");
+        System.out.println("commit test number 4");
     }
 }
