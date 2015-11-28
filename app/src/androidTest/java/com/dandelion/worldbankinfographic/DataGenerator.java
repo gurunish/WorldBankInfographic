d4f2fd7ac46065bc7eed71360dea803488b464f0
Mert Ruso <mert.ruso@kcl.ac.uk>
2015-11-28 15:57:35 +0000

    Made arrays holding all the ids and names of countries in Europe (in same order), should be useful to iterate through later

---
+++ app/src/androidTest/java/com/dandelion/worldbankinfographic/DataGenerator.java
@@ -0,0 +1,18 @@
+package com.dandelion.worldbankinfographic;
+
+/**
+ * Created by M on 28/11/2015.
+ */
+public class DataGenerator {
+    private static String[] countryIds = {"ALB","AND","ARM","AUT","AZE","BLR","BEL","BIH","BGR",
+    "HRV","CYP","CZE","DNK","EST","FRO","FIN","FRA","GEO","DEU","GIB","GRC","HUN","ISL","IRL","ISR",
+    "ITA","KAZ","LVA","LIT","LTU","LUX","MKD","MLT","MDA","MCO","NLD","NOR","POL","PRT","ROM","RUS",
+    "SMR","SVK","SVN","ESP","SWE","CHE","TUR","UKR","GBR"};
+    private static String[] countryNames = {"Albania","Andorra","Armenia","Austria","Azerbaijan",
+    "Belarus","Belgium","Bosnia & Herzegovina","Bulgaria","Croatia","Cyprus","Czech Republic",
+    "Denmark","Estonia","Faroe Islands","Finland","France","Georgia","Germany","Gibraltar","Greece",
+    "Hungary","Iceland","Ireland","Israel","Italy","Kazakhstan","Latvia","Liechtenstein","Lithuania",
+    "Luxembourg","Macedonia","Malta","Moldova","Monaco","Netherlands","Norway","Poland","Portugal",
+    "Romania","Russia","San Marino","Slovakia","Slovenia","Spain","Sweden","Switzerland","Turkey",
+    "Ukraine","United Kingdom"};
+}
