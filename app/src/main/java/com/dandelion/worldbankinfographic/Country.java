45f224352299e5a2511fad628248e99fa4208e6e
HazikAnwar <hazik.anwar@kcl.ac.uk>
2015-12-04 15:24:50 +0000

    JSON is now parsed, saves as country object

--- app/src/main/java/com/dandelion/worldbankinfographic/Country.java
+++ app/src/main/java/com/dandelion/worldbankinfographic/Country.java
@@ -2,12 +2,21 @@ package com.dandelion.worldbankinfographic;
 
 public class Country {
 
-    private String id;
     private String name;
+    private double[] values;
 
-    public Country(String countryCode,String countryName){
-        id=countryCode;
+    //'value' is a double array where value[0] is 2013, value[1] is 2012..... value[9] = 2004.
+    public Country(String countryName, double[] unemploymentValues){
         name = countryName;
+        values = unemploymentValues;
+    }
+
+    public double[] getValues(){
+        return values;
+    }
+
+    public String getName(){
+        return name;
     }
 
 }
--- app/src/main/java/com/dandelion/worldbankinfographic/MainActivity.java
+++ app/src/main/java/com/dandelion/worldbankinfographic/MainActivity.java
@@ -6,35 +6,35 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.InputStream;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG_VALUE = "value";
+    double unemploymentRate[] = new double[10];
     String downloadData = "";
     Spinner spinnerCountry;
+    String url;
+    Country[] countries = new Country[50];
 
-    //JSON tags
-    private static final String TAG_PAGE = "page";
-    private static final String TAG_PAGES = "pages";
-    private static final String TAG_PER_PAGE = "per_page";
-    private static final String TAG_TOTAL = "total";
-    private static final String TAG_INDICATOR = "indicator";
-    private static final String TAG_INDICATOR_ID = "id";
-    private static final String TAG_INDICATOR_VALUE = "value";
-    private static final String TAG_COUNTRY = "country";
-    private static final String TAG_COUNTRY_ID = "id";
-    private static final String TAG_COUNTRY_VALUE = "value";
-    private static final String TAG_VALUE = "value";
-    private static final String TAG_DECIMAL = "decimal";
-    private static final String TAG_DATE = "date";
+    private static String[] countryID = {"ALB","AND","ARM","AUT","AZE","BLR","BEL","BIH","BGR",
+            "HRV","CYP","CZE","DNK","EST","FRO","FIN","FRA","GEO","DEU","GIB","GRC","HUN","ISL","IRL","ISR",
+            "ITA","KAZ","LVA","LIT","LTU","LUX","MKD","MLT","MDA","MCO","NLD","NOR","POL","PRT","ROM","RUS",
+            "SMR","SVK","SVN","ESP","SWE","CHE","TUR","UKR","GBR"};
 
-    //private static String url = "http://api.worldbank.org/countries/all/indicators/SL.UEM.TOTL.ZS?per_page=3000&date=2004:2013&format=json";
-    private static String url = "http://api.worldbank.org/countries/GBR/indicators/SL.UEM.TOTL.ZS?per_page=3000&date=2013&format=json";
-    //JSONArray page = null;
+    private static String[] countryNames = {"Albania","Andorra","Armenia","Austria","Azerbaijan",
+            "Belarus","Belgium","Bosnia & Herzegovina","Bulgaria","Croatia","Cyprus","Czech Republic",
+            "Denmark","Estonia","Faroe Islands","Finland","France","Georgia","Germany","Gibraltar","Greece",
+            "Hungary","Iceland","Ireland","Israel","Italy","Kazakhstan","Latvia","Liechtenstein","Lithuania",
+            "Luxembourg","Macedonia","Malta","Moldova","Monaco","Netherlands","Norway","Poland","Portugal",
+            "Romania","Russia","San Marino","Slovakia","Slovenia","Spain","Sweden","Switzerland","Turkey",
+            "Ukraine","United Kingdom"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,51 +47,63 @@ public class MainActivity extends AppCompatActivity {
         spinnerCountry.setAdapter(adapterCountry);
 
         //Execute Asynctask to start JSON parsing of the URL stated above
-        new DownloadData().execute(url);
+        for (int i = 0; i < countryID.length; i++){
+            url = "http://api.worldbank.org/countries/" + countryID[i] + "/indicators/SL.UEM.TOTL.ZS?per_page=3000&date=2004:2013&format=json";
+            new DownloadData().execute(url);
+            countries[i] = new Country(countryNames[i], unemploymentRate);
+            Toast.makeText(getApplicationContext(), "Successfully downloaded data for " + countries[i].getName(), Toast.LENGTH_SHORT).show();
+        }
     }
 
     /**
      * This class executes codes in the background
      */
-    private class DownloadData extends AsyncTask<String, Void, String> {
+    private class DownloadData extends AsyncTask<String,Double,JSONArray> {
 
-        private String readData(String urlName) throws IOException {
-            StringBuffer buffer = new StringBuffer();
-            URL url = new URL(urlName);
-            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.setDoInput(true);
-            connection.connect();
-            BufferedReader in;
-            in = new BufferedReader( new InputStreamReader(connection.getInputStream()));
-            String inputLine = in.readLine();
-            while (inputLine != null) {
-                buffer.append(inputLine);
-                inputLine = in.readLine();
+
+        @Override
+        protected JSONArray doInBackground(String... params) {
+            if(params.length!=1) return null;
+            try{
+                URL url = new URL(params[0]);
+                InputStream is = url.openStream();
+                DataInputStream dataInputStream = new DataInputStream(is);
+                byte[] buffer = new byte[1024];
+                int bufferLength;
+                ByteArrayOutputStream output = new ByteArrayOutputStream();
+                while((bufferLength = dataInputStream.read(buffer))>0){
+                    output.write(buffer,0,bufferLength);
+
+                }
+                downloadData = output.toString();
+            }catch (Exception e){
+                e.printStackTrace();
+                Toast.makeText(getApplicationContext(), "Error downloading data", Toast.LENGTH_LONG).show();
             }
-            in.close();
-            connection.disconnect();
-            return(buffer.toString());
-        }
-
-        protected String doInBackground(String... urls) {
-
             try {
-                String data = readData(urls[0]);
-                return data;
-            } catch (IOException e) {
-                Log.e("DownloadData", "Error when downloading from URL");
-                return "Error occurred when reading data from URL";
+                return new JSONArray(downloadData);
             }
+            catch(Exception e){
+
+            }
+            return null;
         }
 
-        protected void onPostExecute(String result) {
-            downloadData = result;
-            Log.d("DownloadData", "Data has been downloaded.");
-            Toast.makeText(getApplicationContext(), "Data has been successfully downloaded", Toast.LENGTH_LONG).show();
-
+        @Override
+        public void onPostExecute(JSONArray downloadData) {
+            if(downloadData!=null)
+                try{
+                    JSONArray countryList = downloadData.getJSONArray(1);
+                    JSONObject country;
+                    for(int i = 0; i < countryList.length(); i++){
+                        country = countryList.getJSONObject(i);
+                        String unemploymentString = country.getString(TAG_VALUE);
+                        double unemploymentValue = Double.parseDouble(unemploymentString);
+                        unemploymentRate[i] = unemploymentValue;
+                    }
+                }catch (Exception e){
+                }
         }
-
     }
 
 }
