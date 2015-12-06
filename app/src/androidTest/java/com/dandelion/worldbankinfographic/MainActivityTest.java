91c0e36829ce88c345af7f39a038368dcbfb9bcf
gurunish <i.dunt.know@mail.com>
2015-12-06 14:48:04 +0000

    Trying to test Spinner GUI

--- app/src/androidTest/java/com/dandelion/worldbankinfographic/MainActivityTest.java
+++ app/src/androidTest/java/com/dandelion/worldbankinfographic/MainActivityTest.java
@@ -6,12 +6,15 @@ import android.test.ActivityInstrumentationTestCase2;
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+    MainActivity mainActivity;
+
     public MainActivityTest() {
         super(MainActivity.class);
+        mainActivity = getActivity();
     }
 
     public void testActivityExists() {
-        MainActivity mainActivity = getActivity();
         assertNotNull(mainActivity);
     }
+
 }
