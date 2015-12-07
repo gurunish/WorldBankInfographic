b85960e4de4f75204dc9654c5772bd56621b664d
HazikAnwar <hazik.anwar@kcl.ac.uk>
2015-12-07 14:49:46 +0000

    Update Master Class

--- app/src/main/java/com/dandelion/worldbankinfographic/MainActivity.java
+++ app/src/main/java/com/dandelion/worldbankinfographic/MainActivity.java
@@ -5,15 +5,10 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //test commit
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("committ test numbe 2");
-        System.out.println("commit test number 3");
-        System.out.println("commit test number 4");
     }
 }
